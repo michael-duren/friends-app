0695d05e8476e0025210dbcb1374f7b78a47370c
Michael Duren <michaeld@michaelduren.com>
2023-04-11 13:42:40 -0500

    started sketching entity setup

---
+++ Model/Friend.cs
@@ -0,0 +1,10 @@
+namespace FriendsApp.FriendModel
+{
+		public class FriendModel
+		{
+				public int Id { get; set; }
+				public string Name { get; set; }
+				public string Email { get; set; }
+				public string Phone { get; set; }
+		}
+}
